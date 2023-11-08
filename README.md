3bce09bb793b0b411d14192e67b3ff41af7589f9
beeb <703631+beeb@users.noreply.github.com>
2023-11-08 16:12:28 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,10 @@
+# Example with Rust and Axum on AWS Lambda
+
+This project is best developed with [Cargo Lambda](https://www.cargo-lambda.info/guide/getting-started.html).
+
+To serve the lambda on port 9000 with auto-reloading for development, run:
+
+```bash
+cargo lambda watch
+```
+
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Flake for development in nix dev shell";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    # Rust
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rust-analyzer-unwrapped
+            toolchain
+          ];
+
+          # Environment variables
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+        };
+      });
+}
